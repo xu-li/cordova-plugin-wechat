6cc1295fcc88028efa8cca7a8fb31b08a4cdaa79
xu.li <AthenaLightenedMyPath@gmail.com>
2015-11-29 11:46:40 +0800

    fixes #112

--- scripts/android-install.js
+++ scripts/android-install.js
@@ -1,12 +1,20 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-    var path         = context.requireCordovaModule('path'),
-        fs           = context.requireCordovaModule('fs'),
-        shell        = context.requireCordovaModule('shelljs'),
-        projectRoot  = context.opts.projectRoot,
-        ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser,
-        config       = new ConfigParser(path.join(context.opts.projectRoot, "config.xml")),
+    var path        = context.requireCordovaModule('path'),
+        fs          = context.requireCordovaModule('fs'),
+        shell       = context.requireCordovaModule('shelljs'),
+        projectRoot = context.opts.projectRoot;
+
+    var ConfigParser = null;
+    try {
+        ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser;
+    } catch(e) {
+        // fallback
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser');
+    }
+    
+    var config      = new ConfigParser(path.join(context.opts.projectRoot, "config.xml")),
         packageName = config.android_packageName() || config.packageName();
 
     console.info("Running android-install.Hook: " + context.hook + ", Package: " + packageName + ", Path: " + projectRoot + ".");
