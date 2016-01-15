2293782e36326497e73d10bd99adc8b8b334c25c
xu.li <AthenaLightenedMyPath@gmail.com>
2016-01-15 16:17:39 +0800

    fixes #121 ios thumbnail size issue

--- src/ios/CDVWechat.m
+++ src/ios/CDVWechat.m
@@ -8,6 +8,8 @@
 
 #import "CDVWechat.h"
 
+static int const MAX_THUMBNAIL_SIZE = 320;
+
 @implementation CDVWechat
 
 #pragma mark "API"
@@ -347,7 +349,35 @@
 - (UIImage *)getUIImageFromURL:(NSString *)url
 {
     NSData *data = [self getNSDataFromURL:url];
-    return [UIImage imageWithData:data];
+    UIImage *image = [UIImage imageWithData:data];
+    
+    if (image.size.width > MAX_THUMBNAIL_SIZE || image.size.height > MAX_THUMBNAIL_SIZE)
+    {
+        CGFloat width = 0;
+        CGFloat height = 0;
+        
+        // calculate size
+        if (image.size.width > image.size.height)
+        {
+            width = MAX_THUMBNAIL_SIZE;
+            height = width * image.size.height / image.size.width;
+        }
+        else
+        {
+            height = MAX_THUMBNAIL_SIZE;
+            width = height * image.size.width / image.size.height;
+        }
+        
+        // scale it
+        UIGraphicsBeginImageContext(CGSizeMake(width, height));
+        [image drawInRect:CGRectMake(0, 0, width, height)];
+        UIImage *scaled = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        return scaled;
+    }
+    
+    return image;
 }
 
 - (void)successWithCallbackID:(NSString *)callbackID
